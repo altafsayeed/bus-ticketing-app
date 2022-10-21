5afb1b0d629326953a2854114a78c1b950aec6f3
altafsayeed <altafsayeed19@yahoo.com>
2022-10-20 23:31:46 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+import "antd/dist/antd.min.css";
+
+function App() {
+  return (
+    <div className="App m-5">
+      <h1>Bus Ticketing App</h1>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
