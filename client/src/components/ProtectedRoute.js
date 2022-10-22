40a9cad49d6747aa2b76b935d175c31f421316b2
altafsayeed <altafsayeed19@yahoo.com>
2022-10-21 23:53:49 -0700

    authorization done

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { message } from "antd";
+
+function ProtectedRoute({ children }) {
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const validateToken = async () => {
+    try {
+      const response = await axios.post(
+        "/api/users/get-user-by-id",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (response.data.success) {
+        setLoading(false);
+      } else {
+        setLoading(false);
+        localStorage.removeItem("token");
+        message.error(response.data.message);
+        navigate("/login");
+      }
+    } catch (error) {
+      localStorage.removeItem("token");
+      message.error(error.message);
+      setLoading(false);
+      navigate("/login");
+    }
+  };
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      validateToken();
+    } else {
+      navigate("/login");
+    }
+  });
+
+  return <div>{loading ? <div>Loading... </div> : <>{children}</>}</div>;
+}
+
+export default ProtectedRoute;
